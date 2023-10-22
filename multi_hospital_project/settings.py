f341fb440f26ad24041985e7d7aaa58f8c7ce823
sanjay <sanjaypython22@gmail.com>
2023-10-22 18:24:47 +0530

    p

--- multi_hospital_project/settings.py
+++ multi_hospital_project/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'multi_hospital',
-        'USER': '	multi_hospital_user',
+        'USER': 'multi_hospital_user',
         'PASSWORD': 'qwerty123',
         'HOST': 'localhost',
         'PORT': '3306',
