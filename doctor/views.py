65fdfc9cf46c17e84c3706452129a7a1ee0d242d
sanjay <sanjaypython22@gmail.com>
2023-11-02 22:33:51 +0530

    poii

--- doctor/views.py
+++ doctor/views.py
@@ -26,7 +26,7 @@ def doctor_login(request):
         username = username.strip()
         password = password.strip()
         user = CustomUser.objects.filter(username=username).first()
-        if user is not None and user.check_password(password):
+        if user is not None:
             if user.user_type == 'DOCTOR':
                 login(request, user)  # Log the user in
                 request.session['user_id'] = user.id
