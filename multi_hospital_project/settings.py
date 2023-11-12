22af3f278a7ebde95b131994015d2337dc45632c
sanjay <sanjaypython22@gmail.com>
2023-11-12 11:06:49 +0530

    oiuytrdsa

--- multi_hospital_project/settings.py
+++ multi_hospital_project/settings.py
@@ -110,12 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-]
-
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
