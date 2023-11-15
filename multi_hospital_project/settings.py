a473cac0c1b3196ad3daa0f11db174f198d5ee34
sanjay <sanjaypython22@gmail.com>
2023-11-15 12:59:49 +0530

    kiuyfd

--- multi_hospital_project/settings.py
+++ multi_hospital_project/settings.py
@@ -72,24 +72,24 @@ WSGI_APPLICATION = 'multi_hospital_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'multi_hospi',
-#         'USER': 'root',
-#         'PASSWORD': 'Hello123456#',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'multi_hospi',
+        'USER': 'root',
+        'PASSWORD': 'Hello123456#',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 # Password validation
